95a31af57f9bc57ce237835c52c785122ab5523f
Niklas <niklas@long.ch>
2022-02-09 16:27:51 +0100

    doc: add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,37 @@
+//! Spectre is a small toolkit for analysing p2p network graphs, though it can also apply more generally
+//! to undirected graphs.
+//!
+//! ## Basic usage
+//!
+//! The library is centered around the [`Graph`](graph::Graph) structure which can be constructed
+//! from one or more [`Edge`](edge::Edge) instances. Once constructed, various measurements and
+//! matrix representations of the graph can be computed.
+//!
+//! ```rust
+//! use spectre::graph::Graph;
+//! use spectre::edge::Edge;
+//!
+//! // Construct the graph instance.
+//! let mut graph = Graph::default();
+//!
+//! // Insert some edges, note the IDs can be any type that is `Copy + Eq + Hash + Ord`.
+//! graph.insert(Edge::new("a", "b"));
+//! graph.insert(Edge::new("a", "c"));
+//!
+//! // Compute some metrics on that state of the graph.
+//! let density = graph.density();
+//! let degree_centrality_delta = graph.degree_centrality_delta();
+//!
+//! // Matrices can be pretty printed...
+//! println!("{}", graph.laplacian_matrix());
+//! // ...outputs:
+//! //
+//! //  ┌          ┐
+//! //  │  2 -1 -1 │
+//! //  │ -1  1  0 │
+//! //  │ -1  0  1 │
+//! //  └          ┘
+//! ```
+
 pub mod edge;
 pub mod graph;
